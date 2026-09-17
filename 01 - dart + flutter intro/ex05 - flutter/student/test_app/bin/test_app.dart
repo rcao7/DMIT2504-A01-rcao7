83613c484dd3115f6a0fa8e8a5e2c36c9601dea9
Robert Cao <rcao7@nait.ca>
2026-09-17 11:41:08 -0600

    delete files for flutter

--- 01 - dart + flutter intro/ex05 - flutter/student/test_app/bin/test_app.dart
+++
@@ -1,5 +0,0 @@
-import 'package:test_app/test_app.dart' as test_app;
-
-void main(List<String> arguments) {
-  print('Hello world: ${test_app.calculate()}!');
-}
--- 01 - dart + flutter intro/ex05 - flutter/student/test_app/test/test_app_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:test_app/test_app.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
